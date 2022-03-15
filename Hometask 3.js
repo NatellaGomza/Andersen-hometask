7abf6feeddbb0bf9a389b6fcf62442faaf6f7ad0
NatellaGomza <gomza.natella@gmail.com>
2022-03-15 17:08:28 +0300

    Fixing reduce

---
+++ Hometask 3.js
@@ -0,0 +1,28 @@
+function myFilter(arr, callback, thisArg) {
+    const filteredArr = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        const callbackWithCalledThis = callback.call(thisArg, arr[i], i, arr);
+        if (callbackWithCalledThis) {
+            filteredArr.push(arr[i]);
+        }
+    }
+
+    return filteredArr;
+}
+
+function myForEach(arr, callback, thisArg) {
+    for (let i = 0; i < arr.length; i++) {
+        callback.call(thisArg, arr[i], i, arr);
+    }
+}
+
+function myReduce(arr, callback, startValue) {
+    let result = startValue;
+
+    for (let i = 0; i < arr.length; i++) {
+        result = callback(result, arr[i], i, arr);
+    }
+
+    return result;
+}
